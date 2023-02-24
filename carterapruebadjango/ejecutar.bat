2165ccc79f0143d62b6bb3d77a1eb4898844d713
Chelsy Clarine Mena Gonzalez <chelsy.mena@andercol.com.co>
2023-02-24 08:41:11 -0500

    agregue en la carpeta raiz el archivo a ejecutar

---
+++ carterapruebadjango/ejecutar.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+CALL "D:\Users\chelsy.mena\OneDrive - Centro de Servicios Mundial SAS\Documentos\Proyectos\Cartera\PortalWeb_Django\venv\Scripts\activate"
+cd "D:\Users\chelsy.mena\OneDrive - Centro de Servicios Mundial SAS\Documentos\Proyectos\Cartera\PortalWeb_Django\carterapruebadjango"
+py manage.py runserver
+
+:: cmd /k "cd /d D:\Users\chelsy.mena\OneDrive - Centro de Servicios Mundial SAS\Documentos\Proyectos\Cartera\PortalWeb_Django\venv\Scripts & activate & cd /d py manage.py runserver"
